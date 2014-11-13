b64fadd6a241f2eb9492fdcac115e5080007d077
Marielle Foster <marielleefoster@gmail.com>
2014-11-13 11:31:41 -0600

    abundant numbers

--- number_theory/abundant.py
+++ number_theory/abundant.py
@@ -1,6 +1,15 @@
 def abundant():
-	for i in range(1000):
+	for i in range(2, 1000):
+		if i < sum_divisors(i):
+			print "abundant: " + str(i)
+			if i%2 == 1:
+				print "ODD"
 
-def sum_divisors():
+def sum_divisors(n):
+	summ = 0
+	for i in range(1, n/2+1):
+		if n%i == 0:
+			summ += i
+	return summ
 
 abundant()
