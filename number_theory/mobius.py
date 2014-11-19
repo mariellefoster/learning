a7069812da142fbcd5238194e18e65b37da6fed1
Marielle Foster <marielleefoster@gmail.com>
2014-11-19 15:01:49 -0600

    calculates runs of numbers with square factors

--- number_theory/mobius.py
+++ number_theory/mobius.py
@@ -18,19 +18,23 @@ def main():
 	i = 3
 	
 	mstr = []
-	while i < 100000:
-		mobius = False
-		factors = prime_factors(i)
-		for entry in factors:
-			if entry[1] > 1:
-				mobius = True
-				mstr.append(i)
-				break
-		if mobius == False and len(mstr) > 0:
-			if len(mstr) > 4:
-				print mstr	
-			mstr = []
-		i += 1
+	# while i < 100000:
+	# 	mobius = False
+	# 	factors = prime_factors(i)
+	# 	for entry in factors:
+	# 		if entry[1] > 1:
+	# 			mobius = True
+	# 			mstr.append(i)
+	# 			break
+	# 	if mobius == False and len(mstr) > 0:
+	# 		if len(mstr) > 4:
+	# 			print mstr	
+	# 		mstr = []
+	# 	i += 1
+
+	lst = [96675, 96676, 96677, 96678, 96679, 96680]
+	for entry in lst:
+		print entry, prime_factors(entry)
 
 		
 
