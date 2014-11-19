f5114f2e1a5f4db1314be441d965b5dd46a60ca5
Marielle Foster <marielleefoster@gmail.com>
2014-11-18 23:53:40 -0600

    number theory and computer security code

---
+++ number_theory/mobius.py
@@ -0,0 +1,38 @@
+def prime_factors(i):
+	n = 2
+	power = 0
+	lst = []
+	num = i
+	while n <= i/2:
+		while i%n == 0:
+			power += 1
+			i = i/n
+		if power > 0:
+			lst.append([n, power])
+		power = 0
+		n += 1
+	return lst
+
+
+def main():
+	i = 3
+	
+	mstr = []
+	while i < 100000:
+		mobius = False
+		factors = prime_factors(i)
+		for entry in factors:
+			if entry[1] > 1:
+				mobius = True
+				mstr.append(i)
+				break
+		if mobius == False and len(mstr) > 0:
+			if len(mstr) > 4:
+				print mstr	
+			mstr = []
+		i += 1
+
+		
+
+
+main()
