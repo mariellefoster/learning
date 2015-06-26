2ee4d948af01c773193a56e64873c41ec25f46d6
Mar Foster <marielleefoster@gmail.com>
2015-06-26 14:30:37 -0500

    chapter 2 notes

---
+++ haskell/chap2.hs
@@ -0,0 +1,14 @@
+--Marielle Foster 2015 Notes and Code
+
+-- :t tells you what type something is
+
+
+-- The Eq type class are types that support equality testing
+-- (==) :: Eq a => a -> a -> Bool
+-- (/=) :: Eq a => a -> a -> Bool
+
+
+-- succ :: Enum a => a -> a
+-- head :: [a] -> a
+-- fst :: (a, b) -> a
+-- last :: [a] -> a
